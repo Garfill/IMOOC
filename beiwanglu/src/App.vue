b7d57973669f396498efbd60a5f4b8d419d31d8a
Garfill <junhui7guan@163.com>
2018-03-14 10:51:37 +0800

    change a id

--- beiwanglu/src/App.vue
+++ beiwanglu/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app1">
     <app-header></app-header>
     <app-content></app-content>
   </div>
@@ -23,7 +23,7 @@ export default {
     margin: 0;
     padding: 0;
   }
-#app {
+#app1 {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;

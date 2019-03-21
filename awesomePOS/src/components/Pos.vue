ec16a16a7e1f2837b83864684c43e344cf5d2c5d
Garfill <junhui7guan@163.com>
2019-03-22 01:26:55 +0800

    start for focutPic

---
+++ awesomePOS/src/components/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pos-pannel-container">
+        <nav-pannel></nav-pannel>
+        <!-- <order-pannel></order-pannel> -->
+        <!-- <food-pannel></food-pannel> -->
+    </div>
+</template>
+
+<script>
+import NavPannel from "@/components/common/NavPannel.vue"
+export default {
+    components:{
+        "nav-pannel":NavPannel,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- beiwanglu/src/components/Total.vue
+++ beiwanglu/src/components/Total.vue
@@ -6,45 +6,41 @@
 </template>
 
 <script>
-	import Date from './Date.vue'
-	import Write from './Write.vue'
+import Date from "./Date.vue";
+import Write from "./Write.vue";
 
-	export default  {
-		name:'Total',
-		components:{
-			'date':Date,
-			'write':Write
-		},
-		methods:{
-			add:function(data){
-				var num = data.data;
-				var plan = data.plan;
-				// console.log(this.week[num].msg);
-				this.week[num].msgs.push(plan);
-			},
-			del:function(id){
-				this.week[id].msgs = ["ok"];
-			}
-		},
-		data:function(){
-			return {
-					week:[
-              {day:'Monday',id:'0',msgs:[]},
-              {day:'Tuesday',id:'1',msgs:[]},
-              {day:'Wednesday',id:'2',msgs:[]},
-              {day:'Thursday',id:'3',msgs:[]},
-              {day:'Friday',id:'4',msgs:[]},
-              {day:'Saturday',id:'5',msgs:[]},
-              {day:'Sunday',id:'6',msgs:[]}
-					]
-
-			}
-		}
-	}
-	
-
+export default {
+    name: "Total",
+    components: {
+        date: Date,
+        write: Write
+    },
+    methods: {
+        add: function(data) {
+            var num = data.data;
+            var plan = data.plan;
+            // console.log(this.week[num].msg);
+            this.week[num].msgs.push(plan);
+        },
+        del: function(id) {
+            this.week[id].msgs = ["ok"];
+        }
+    },
+    data: function() {
+        return {
+            week: [
+                { day: "Monday", id: "0", msgs: [] },
+                { day: "Tuesday", id: "1", msgs: [] },
+                { day: "Wednesday", id: "2", msgs: [] },
+                { day: "Thursday", id: "3", msgs: [] },
+                { day: "Friday", id: "4", msgs: [] },
+                { day: "Saturday", id: "5", msgs: [] },
+                { day: "Sunday", id: "6", msgs: [] }
+            ]
+        };
+    }
+};
 </script>
 
 <style scoped>
-
 </style>
